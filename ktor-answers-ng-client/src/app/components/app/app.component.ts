b71a4f907910742516778c140eefbc080260ef01
Garth Gilmour <garth.gilmour@jetbrains.com>
2023-05-09 16:19:30 +0100

    Improve application title

--- ktor-answers-ng-client/src/app/components/app/app.component.ts
+++ ktor-answers-ng-client/src/app/components/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ktor Answers Client (Angular Version)';
+  title = 'Ktor Answers Client';
+  subtitle = "In Angular & Bootstrap"
 }
--- ktor-answers-ng-client/src/app/components/header/navigation-bar/navigation-bar.component.ts
+++ ktor-answers-ng-client/src/app/components/header/navigation-bar/navigation-bar.component.ts
@@ -7,4 +7,5 @@ import {Component, Input} from '@angular/core';
 })
 export class NavigationBarComponent {
   @Input() title: String | null = null;
+  @Input() subtitle: String | null = null;
 }
